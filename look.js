5ba06a1a119987e16fa1c9192e93fd0fcc0a2916
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-24 09:09:12 +0600

    reduce array function

--- look.js
+++ look.js
@@ -1,19 +1,39 @@
-let findUserById=(users,id)=>{
-    let expectedUser=users.find(user=>{
-        // console.log(user)
-        return user.id === id;
-    })
-    if(expectedUser){
-        return expectedUser}
+// let findUserById=(users,id)=>{
+//     let expectedUser=users.find(user=>{
+//         // console.log(user)
+//         return user.id === id;
+//     })
+//     if(expectedUser){
+//         return expectedUser}
 
-   else{
-    return "user not found"
-   }
+//    else{
+//     return "user not found"
+//    }
 
-};
-let users=[
-    {id:1, name:"Rafi"},
-    {id:2, name:"Karim"},
-    {id:3, name:"Manik"}
+// };
+// let users=[
+//     {id:1, name:"Rafi"},
+//     {id:2, name:"Karim"},
+//     {id:3, name:"Manik"}
+// ]
+// console.log(findUserById(users,3));
+
+
+
+
+
+let getCardTotal=(cart)=>{
+
+
+    let total=card.reduce((accumalator,elem)=>{
+    console.log(accumalator,elem);
+    return accumalator+(elem.price*elem.qty);
+    },0);
+    return total;
+}
+
+let card=[
+    {name:"Pen", price:20, qty:3},
+    {name:"NoteBook",price:50,qty:2}
 ]
-console.log(findUserById(users,3))
+console.log(getCardTotal(card));
